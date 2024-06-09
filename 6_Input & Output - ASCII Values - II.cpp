42e82d6d3328f5d09616db5d340cc81d92aab6e5
SUSHANTH R HEGDE <120232856+SushanthRHegde@users.noreply.github.com>
2024-06-09 19:33:48 +0530

    Create 6_Input & Output - ASCII Values - II.cpp

---
+++ 6_Input & Output - ASCII Values - II.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int alphabet;
+    //entering the ascii number
+    cin>>alphabet;
+    
+    cout<<static_cast<char>(alphabet);
+    //printing the charecter of given ascii number
+    return 0;
+}
