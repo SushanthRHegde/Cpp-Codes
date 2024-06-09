dc64a6d750890e4b3855b48bac422dbfe7c1cd47
SUSHANTH R HEGDE <120232856+SushanthRHegde@users.noreply.github.com>
2024-06-09 19:30:26 +0530

    Create 5_Input & Output - ASCII Values - I.cpp

---
+++ 5_Input & Output - ASCII Values - I.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() { 
+    char alphabet;
+    cin>>alphabet;
+    cout<<static_cast<int>(alphabet);
+    //using static_cast we can get the ascii vale of the given charecter 
+    return 0;
+}
